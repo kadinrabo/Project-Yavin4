9a1aec12e4d7de6741d548130c39e19d86dab34e
kadinrabo <kadinjrabo@gmail.com>
2021-06-19 10:54:22 -0700

    Refactor network service. Moved snapshot into reloadData() method to avoid section identifier count error.

--- Yavin4/Controller/StageController.swift
+++ Yavin4/Controller/StageController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 
 // ######################################################
@@ -16,7 +19,7 @@ class StageController: UICollectionViewController {
     
     var sections = [Section]()
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!  // Data source for configured cells
-    var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()  // Empty data source snapshot for sections
+    
     var stageIdentifier: String!  // Used to filter out sections not selected
 
     override func viewDidLoad() {
@@ -28,20 +31,7 @@ class StageController: UICollectionViewController {
         createDataSource()  // Configure cells
         
         // Network Service
-        guard let sections = NetworkService.getStageSections(with: stageIdentifier) else {  // Get available sections with the stage identifier passed from StagesController
-            if let vc = self.storyboard?.instantiateViewController(identifier: "Error") as? ErrorController {
-                vc.errorText = "guard let sections block @ StageController.swift"
-                self.navigationController?.pushViewController(vc, animated: true)
-            }
-            return
-        }
-        
-        if sections.isEmpty {
-            let ac = Helper.createAlert(title: "Error", message: "Error displaying data. [7]")
-            present(ac, animated: true)
-            return
-        }
-        reloadData(with: sections)
+        networkService()
     }
 }
 
@@ -66,7 +56,9 @@ extension StageController {
         if let vc = self.storyboard?.instantiateViewController(withIdentifier: "Workout") as? WorkoutController {  // Show workout view controller by passing a workout identifier as a filter and a title
             vc.workoutIdentifier = identifier
             vc.title = title
-            self.navigationController?.pushViewController(vc, animated: true)
+            vc.downButton.tintColor = UIColor.label
+            self.collectionView.deselectItem(at: indexPath, animated: true)
+            self.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
         } else {  // Block ran if the instantiation fails
             let ac = Helper.createAlert(title: "Error", message: "Problem displaying requested data. [10]")
             present(ac, animated: true)
@@ -82,10 +74,51 @@ extension StageController {
 extension StageController {
     
     func reloadData(with sections: [Section]) {  // Upload changes to the snapshot with sections
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()  // Empty data source snapshot for sections
         snapshot.appendSections(sections)
         for section in sections {
             snapshot.appendItems(section.data!, toSection: section)
         }
         dataSource?.apply(snapshot, animatingDifferences: true)
+    }
+    
+    func networkService() {
+        Firestore.firestore().collection("yavin4-users").document(Auth.auth().currentUser!.uid).addSnapshotListener { document, error in
+            let result = Result {
+                try document?.data(as: User.self)
+            }
+            switch result {
+            case .success(let data):
+                if let data = data {
+                    if !data.paid {
+                        self.sections = NetworkService.getFreeStageSections(with: self.stageIdentifier)!
+                        if self.sections.isEmpty {
+                            let ac = Helper.createAlert(title: "Error", message: "Error displaying data. [6]")
+                            self.present(ac, animated: true)
+                            return
+                        }
+                        self.reloadData(with: self.sections)
+                    } else if data.paid {
+                        self.sections = NetworkService.getStageSections(with: self.stageIdentifier)!
+                        if self.sections.isEmpty {
+                            let ac = Helper.createAlert(title: "Error", message: "Error displaying data. [7]")
+                            self.present(ac, animated: true)
+                            return
+                        }
+                        self.reloadData(with: self.sections)
+                    } else {
+                        if let vc = self.storyboard?.instantiateViewController(identifier: "Error") as? ErrorController {
+                            vc.errorText = "viewDidLoad() @ StageController.swift in else block of success case"
+                            self.navigationController?.pushViewController(vc, animated: true)
+                        }
+                    }
+                }
+            case .failure(_):
+                if let vc = self.storyboard?.instantiateViewController(identifier: "Error") as? ErrorController {
+                    vc.errorText = "viewDidLoad() @ StageController.swift in .failure case block"
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+            }
+        }
     }
 }
